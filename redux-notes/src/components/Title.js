41d51fec96e7d09aa6224b49c79dfe598b782850
Golden Eagle <joowoon.kang@biola.edu>
2020-04-12 21:54:35 -0700

    just added mapStateToProps to remove useReducer. and the states will become props.

--- redux-notes/src/components/Title.js
+++ redux-notes/src/components/Title.js
@@ -1,9 +1,11 @@
-import React, { useState, useReducer } from "react";
+import React, { useState } from "react";
 import { initialState, reducer } from "../reducers/title";
 
-const Title = () => {
+import { connect } from "react-redux" // in order to allow us to connect react component to redux
+
+const Title = (props) => {
   const [newTitle, setNewTitle] = useState();
-  const [state, dispatch] = useReducer(reducer, initialState);
+//   const [state, dispatch] = useReducer(reducer, initialState); since you props.dispatch and others (state.title and state.editing) you don't need this. 
 
   const handleChanges = e => {
     setNewTitle(e.target.value);
@@ -11,24 +13,24 @@ const Title = () => {
 
   const handleEdit = e => {
     e.preventDefault();
-    dispatch({ type: "TOGGLE_EDITING" });
+    props.dispatch({ type: "TOGGLE_EDITING" });
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     setNewTitle("");
-    dispatch({ type: "UPDATE_TITLE", payload: newTitle || state.title });
-    dispatch({ type: "TOGGLE_EDITING" });
+    props.dispatch({ type: "UPDATE_TITLE", payload: newTitle || props.title });
+    props.dispatch({ type: "TOGGLE_EDITING" });
   };
 
   return (
     <div>
-      {state.editing ? (
+      {props.editing ? (
         <form onSubmit={handleSubmit}>
           <input
             type="text"
             name="newTitle"
-            placeholder={state.title}
+            placeholder={props.title}
             value={newTitle}
             onChange={handleChanges}
           />
@@ -36,7 +38,7 @@ const Title = () => {
         </form>
       ) : (
         <div>
-          <h1>{state.title}</h1>
+          <h1>{props.title}</h1>
           <button onClick={handleEdit}>Edit</button>
         </div>
       )}
@@ -44,4 +46,11 @@ const Title = () => {
   );
 };
 
-export default Title;
+function mapStateToProps(state ) {// state can be store too. depends on what you want to name it.
+    return{
+        title: state.title,
+        editing: state.editing
+    }
+}
+
+export default connect(mapStateToProps)(Title);
